e690a8eda034cb7e03dbb4d4a1540c42f744ddfe
woltsu <oltsuwh@gmail.com>
2019-10-29 20:54:06 +0200

    Removed test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-a=""
-
-if [ -z $a ]; then
-  echo "Please input your TELEGRAM_BOT_TOKEN value in the CircleCI settings for this project or provide another environmental variable name as a parameter for 'getchat' command"
-elif [ -z $b ]; then
-  echo "Please input your TELEGRAM_CHAT_ID value in the CircleCI settings for this project or provide another environmental variable name as a parameter for 'getchat' command"
-else
-  echo "lul!"
-fi
